38cb1f3084608d3f4c4d8e8d36df1e32a0924d36
Shubham-2119 <127936038+Shubham-2119@users.noreply.github.com>
2023-06-30 12:06:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+ //Naming Conventions
+
+    // Camelcase
+    let firstName = "Yash";
+    // PascalCase
+    let LastName = "Bothra";
+    //SnakeCase
+    let middle_name = "Raj";
+
+    // Using Constant (const keyworld)
+    const price = 99;
+
+    // Course Name (String)
+    var CourseName = "Java DS/Algo Course";
+
+    // Card Number  (Number)
+    var CardNumber = "1234567890";
+
+    // Payment Status (Boolen)
+    var isPaid = true;
+
+    //Declarin a Variable
+    var modeOfPayment = prompt("mode of Payment?");
+
+    //Output to User
+    alert(firstName + " sucessfully bought " + CourseName + "using" + modeOfPayment);
